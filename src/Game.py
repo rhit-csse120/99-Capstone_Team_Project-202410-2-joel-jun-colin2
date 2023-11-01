535bd49d6029249a1a027317008d0737cdac1b48
meyerja <meyerja@rose-hulman.edu>
2023-11-01 10:55:39 -0400

    Draw ship

--- src/Game.py
+++ src/Game.py
@@ -7,15 +7,16 @@ The  Game (model)  file for the Model-View-Controller architecture for our game.
    typically asks each of the Game's objects to do whatever needs to happen
    independently of events / user-input.
 
-Team members:
+Team members: Joel, Colin, Jun
 """
-# TODO: Put the names of your entire team in the above doc-string.
+# DONE: Put the names of your entire team in the above doc-string.
 
 import pygame
 from Asteroid import Asteroid
 from Asteroids import Asteroids
+from Starship import Ship
 
-# TODO: Put each class in its own module, using the same name for both.
+# DONE: Put each class in its own module, using the same name for both.
 #  Then use statements like the following, but for YOUR classes in YOUR modules:
 #     from Fighter import Fighter
 
@@ -25,20 +26,25 @@ class Game:
         self.screen = screen
         # self.asteroid = Asteroid(screen)
         self.asteroids = Asteroids(screen)
-        # TODO: Store whatever YOUR game needs, perhaps something like this:
+        self.Ship = Ship(screen)
+
+        # DONE: Store whatever YOUR game needs, perhaps something like this:
         #     self.missiles = Missiles(self.screen)
         #     self.fighter = Fighter(self.screen, self.missiles)
 
     def draw_game(self):
         self.asteroids.draw()
+        self.Ship.draw_self()
+
         """ Ask all the objects in the game to draw themselves. """
-        # TODO: Use something like the following, but for objects in YOUR game:
+        # DONE: Use something like the following, but for objects in YOUR game:
         #     self.fighter.draw()
 
     def run_one_cycle(self):
         self.asteroids.move()
         self.asteroids.remove_asteroid()
+
         """ All objects that do something at each cycle: ask them to do it. """
-        # TODO: Use something like the following, but for objects in YOUR game:
+        # DONE: Use something like the following, but for objects in YOUR game:
         #     self.missiles.move()
         #     self.missiles.handle_explosions(self.enemies)
