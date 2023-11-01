fb28465e19c4c29f62f0c81ece92427abfd25971
parks16 <parks16@rose-hulman.edu>
2023-11-01 10:25:00 -0400

    done

---
+++ src/Asteroid.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+
+class Asteroid:
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+        self.speed = random.randint(1, 8)
+        self.image = pygame.image.load("../media/Asteroid_S.gif")
+        self.x = screen.get_width()
+        self.y = random.randint(0, screen.get_height())
+        self.off_screen = False
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        self.x = self.x - self.speed
+        if self.x < -100:
+            self.off_screen = True
--- src/FuelCells.py
+++ src/FuelCells.py
@@ -1,5 +1,24 @@
+import pygame
+import random
+from FuelCell import FuelCell
+
 class FuelCells:
     def __init__(self, screen, x, y, ):
         self.screen = screen
         self.x = x
         self.y = y
+
+        x_first = x
+        y_first = y
+        fuelCells_list = []
+        if random.random() < 0.01:  # Adjust the probability as needed
+            x_first = random.randint(0, screen.width)
+            y_first = random.randint(0, screen.height)
+            fuelCells_list.append(FuelCell(screen, x_first, y_first))
+
+    def draw(self):
+        for fuelCell in self.list_of_fuelCells:
+            fuelCell.draw()
+
+    def remove_passed_cells(self):          # Do I even need this?
+        pass
