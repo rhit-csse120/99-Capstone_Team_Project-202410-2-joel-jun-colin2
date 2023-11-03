d9bfc3933ae2e314eb3818cc91e4a0cca716d589
meyerja <meyerja@rose-hulman.edu>
2023-11-03 11:11:26 -0400

    Modify starship size and create time variable

--- src/Starship.py
+++ src/Starship.py
@@ -8,6 +8,8 @@ class Ship:
     def __init__(self, screen: pygame.Surface, speed=5):
         self.screen = screen
         self.image = pygame.image.load("../media/Starship.png")
+        size = (125, 50)
+        self.image = pygame.transform.scale(self.image, size)
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
