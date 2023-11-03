41051cf1153d87fdf5a60d7b3ed4af51991b7d4a
parks16 <parks16@rose-hulman.edu>
2023-11-03 01:22:19 -0400

    If the ship consumes the fuelcells, they are disappeared

--- src/FuelCell.py
+++ src/FuelCell.py
@@ -1,20 +1,23 @@
 import pygame
 from Starship import Ship
+import random
 
 class FuelCell:
-    def __init__(self, screen, speed, x=100, y=100):
+    def __init__(self, screen, speed):
         self.screen = screen
         self.image = pygame.image.load("../media/FuelCell.gif")
-        size = (110, 110)
+        size = (120, 120)
         self.image = pygame.transform.scale(self.image, size)
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.x = x
-        self.y = y
+        self.x = random.randint(0, screen.get_width())
+        self.y = random.randint(0, screen.get_height())
         self.speed = speed
         self.is_off_the_screen = False
         self.has_consumed = False
         self.has_charged = False
+        self.score = 100
+        self.time = pygame.time.Clock()
         # self.charge_sound = pygame.mixer.Sound()  # Find a sound
 
     def draw(self):
@@ -28,9 +31,13 @@ class FuelCell:
 
     def move(self):
         self.x -= self.speed
-        if self.y > self.screen.get_height():
+        if self.x < -10:
             self.is_off_the_screen = True
 
     def charge(self):
         self.charge_sound.play()
         self.has_charged = True
+
+    # def update_score(self):
+
+
--- src/FuelCells.py
+++ src/FuelCells.py
@@ -10,47 +10,34 @@ from Starship import Ship
 class FuelCells:
     def __init__(self, screen, ship):
         self.screen = screen
-        self.x_first = screen.get_width() // 2
-        self.y_first = screen.get_height() // 2
-
         self.fuelCells_list = []
         self.ship = ship
-        # if random.random() < 0.01:  # Adjust the probability as needed
-        #     x_first = random.randint(0, screen.width)
-        #     y_first = random.randint(0, screen.height)
-        #     self.fuelCells_list.append(FuelCell(screen, x_first, y_first))
-        for k in range(random.randint(9, 12)):
-            self.fuelCells_list.append(FuelCell(screen, 3, self.x_first, self.y_first))
-            self.x_first = random.randint(0, 1200)
-            self.y_first = random.randint(0, 650)
+        for k in range(random.randint(5, 7)):
+            self.fuelCells_list.append(FuelCell(screen, 3))
 
     def draw(self):
-        # while True:
-        #     for fuelCell in self.fuelCells_list:
-        #         fuelCell.draw()
-        #     if
-        #         break
 
         # for fuelCell in self.fuelCells_list:
         #     events = pygame.event.get()
         #     events = fuelCell.draw()
         #     set_timer(events, 3, loops=1)
-
+        # while True:
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:
+        #             break
+        #     for fuelCell in self.fuelCells_list:
+        #         fuelCell.draw()
         for fuelCell in self.fuelCells_list:
             fuelCell.draw()
+
     def move(self):
         for fuelCell in self.fuelCells_list:
             fuelCell.move()
-    def remove_charged_cells(self):
-        # for fuelCell in self.fuelCells_list:
-        #     fuelCell.is_consumed(self.ship)
-        #     if fuelCell.has_consumed:
-        #         del fuelCell
 
+    def remove_charged_cells(self):
         for k in range(len(self.fuelCells_list) - 1, -1, -1):
             fuelCell = self.fuelCells_list[k]
             fuelCell.is_consumed(self.ship)
-            if fuelCell.has_consumed:
+            if fuelCell.has_consumed or fuelCell.is_off_the_screen:
                 del self.fuelCells_list[k]
-
-
+                self.fuelCells_list.append(FuelCell(self.screen, 3))
