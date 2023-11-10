ae6bd7040a9f6e73faae32f194cf17b23226e489
rippercw <rippecw@rose-hulman.edu>
2023-11-10 00:37:28 -0500

    The enemy is now a UFO! And it works!

--- src/Enemy.py
+++ src/Enemy.py
@@ -5,27 +5,28 @@ import time
 class Enemy:
     def __init__(self, screen: pygame.Surface, ship: Ship):
         self.screen = screen
-        self.image = pygame.image.load("../media/Asteroid_S.gif")
+        self.image = pygame.image.load("../media/ufo.png")
         self.x = screen.get_width()
         self.ship = ship
         self.y = 100
         self.x_speed = 7
         self.stored_time = time.time()
-        self.spawn_time = 20
+        self.spawn_time = 10
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
 
 
     def move(self):
-        pass
-    #     if time.time() >=
-    #         self.x -= self.x_speed
-    #         if self.y < self.ship.y:
-    #             self.y += 2.5
-    #         if self.y > self.ship.y:
-    #             self.y -= 2.5
-    #         if self.x < -100:
-    #             self.x = 1300
+        if time.time() >= self.stored_time + self.spawn_time:
+            self.x -= self.x_speed
+            if self.y < self.ship.y:
+                self.y += 2
+            if self.y > self.ship.y:
+                self.y -= 2
+            if self.x < -100:
+                self.x = 1300
+                self.spawn_time = self.spawn_time + 10
+
 
 
 
